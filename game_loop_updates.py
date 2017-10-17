025f9b306f6edbb456346d980b486bd4e46c4910
noraassab <32856366+noraassab@users.noreply.github.com>
2017-10-16 18:18:27 -0700

    Update game_loop_updates.py

--- game_loop_updates.py
+++ game_loop_updates.py
@@ -39,47 +39,104 @@ def updateCorners(car):
 #updates center of mass location
 def updateCOM(car): #added displaySurface for debug
     theta = math.radians(car["r"])
-    comX = int(car["COMInfo"][1] * (math.cos(car["COMInfo"][0] + theta)) + car["pos"][0])
-    comY = int(car["COMInfo"][1] * -(math.sin(car["COMInfo"][0] + theta)) + car["pos"][1])
+    comX = int(car["COMInfo"][1] * (math.cos(math.radians(car["COMInfo"][0]) + theta)) + car["pos"][0])
+    comY = int(car["COMInfo"][1] * -(math.sin(math.radians(car["COMInfo"][0]) + theta)) + car["pos"][1])
     car["COM"] = (comX, comY)
     debugDrawPointLocation(car["COM"], 3, CYAN) #COM debug
 
 #updates the drawing inside the hitbox
 #def updateCarSprite
 
-def updateCarPosition(car, netVel, tick):
-    pX = car["pos"][0] + (netVel[0] * tick)
-    pY = car["pos"][1] + (netVel[1] * tick)
+def updateCarPosition(car, netVel):
+    pX = car["pos"][0] + (netVel[0] / FPS)
+    pY = car["pos"][1] + (netVel[1] / FPS)
     return (pX,pY)
     
 def updateCarVelocity(car, netAccel):
     vX = car["vel"][0] + (netAccel[0] / FPS)
     vY = car["vel"][1] + (netAccel[1] / FPS)
-#    print(car["vel"])
+    #print(car["vel"])
     return (vX,vY)
-def updateCarRotation(car, tick):
-    car["r"] += car["angV"] / tick
+
+
+def calculateCarNetAccel(car):
+    #netAccel = gravity()
+    netAccel = [0, 0]
+    for inputs in car["forceInputs"]:
+        if inputs == "forward":
+            drivingForce(car, True)
+            print("forward")
+        if inputs == "backward":
+            drivingForce(car, False)
+            print("backward")
+        if inputs == "boost":
+            boostForce(car)
+            print("boost")
+            
+##    normalForce(car)
+##    drivingForce(car)
+##    boostForce(car)
+##    carForce(car)
+##    ballForce(car)
+##    frictionForce(car)
+    
+    for force in car["forces"]:
+        netAccel[0] += force[0][0]
+        netAccel[1] += force[0][1]
+    car["forces"] = []
+    return netAccel
+
+'''
 def calculateCarNetAccel(car, forces):
     netAccel = [0,0]
     boundaryNormalForce(car)
     frictionForce(car)
-    '''
-    for force in forces:
-        if len(force) == 0:
-            continue
-        elif len(force) > 1:
-            for obj in force:
-                netAccel[0] += obj[0]
-                netAccel[1] += obj[1]   
-        else:
-            netAccel[0] += force[0]
-            netAccel[1] += force[1]
-    '''
+    
+##    for force in forces:
+##        if len(force) == 0:
+##            continue
+##        elif len(force) > 1:
+##            for obj in force:
+##                netAccel[0] += obj[0]
+##                netAccel[1] += obj[1]   
+##        else:
+##            netAccel[0] += force[0]
+##            netAccel[1] += force[1]
+    
     netAccel[0] = forces["gravity"][0] + forces["normals"][0] + forces["driving"][0] + forces["boost"][0] + forces["friction"][0]
     netAccel[1] = forces["gravity"][1] + forces["normals"][1] + forces["driving"][1] + forces["boost"][1] + forces["friction"][1]
     return netAccel
+'''
+def updateCarRotation(car, netAngVel):
+    rotation = car["r"] + (netAngVel / FPS)
+    return rotation
 
+def updateCarAngVel(car, netAngAccel):
+    angVel = car["angV"] + (netAngAccel / FPS)
+    #print(car["vel"])
+    return angVel
+
+def calculateCarNetAngAccel(car):
+    r = car["r"]
+    while r > 360 or r < 0:
+        if r > 0:
+            r -= 360
+        else:
+            r += 360
+    alpha = 0
+    for force in car["forces"]:
+        distance = pointDistance(car["pos"], force[1])
+        if distance < MIN_ERROR:
+            continue
+        else:
+            angle = pointAngle(car["pos"], force[1]) - r
+        alpha += torques(car,force,distance,angle)
+    return alpha   
+    
 def boostAnimation(car, toggle = False):
     if toggle:
         for i in range(random.randint(5,10)):
             pass
+
+def setBoostStrength(num):
+    boostStrength = -num
